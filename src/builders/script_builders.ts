ebe07055c29cddc2a534df4960b4e5e7a69015de
Bjarki Ágúst Guðmundsson <bjarki@google.com>
2021-02-16 05:09:50 -0800

    Implement safeScriptWithArgs builder function
    PiperOrigin-RevId: 357695107

--- src/builders/script_builders.ts
+++ src/builders/script_builders.ts
@@ -18,6 +18,10 @@
 import {createScript, unwrapScriptAsString} from '../implementation/script_impl';
 import {assertIsTemplateObject} from '../implementation/safe_string_literal';
 
+type Primitive = number|string|boolean|null;
+type Serializable =
+    Primitive|readonly Serializable[]|{readonly [key: string]: Serializable};
+
 /**
  * Creates a TrustedScript object from a template literal (without any embedded
  * expressions).
@@ -40,4 +44,56 @@ export function script(templateObj: TemplateStringsArray): TrustedScript {
 /** Creates a `TrustedScript` value by concatenating multiple `TrustedScript`s. */
 export function concatScripts(...scripts: TrustedScript[]): TrustedScript {
   return createScript(scripts.map(unwrapScriptAsString).join(''));
+}
+
+/**
+ * Converts a serializable value into JSON that is safe to interpolate into a
+ * script context. In particular it escapes < characters so that a value of
+ * "</script>" doesn't break out of the context.
+ * @param value: The value to serialize.
+ */
+function serializeAsScriptValue(value: Serializable): string {
+  return JSON.stringify(value).replace(/</g, '\\x3c');
+}
+
+/**
+ * Creates a `TrustedScript` object from a template literal (without any embedded
+ * expressions) along with additional arguments that the script should have
+ * access to. These arguments will be JSON-encoded and passed to the script as
+ * a function call.
+ * @example
+ * ```ts
+ * scriptWithArgs`function (name, props) {
+ *  console.log(name + ' is ' + props.age);
+ * }`('Bob', { 'age': 42 })
+ * ```
+ * would return a `TrustedScript` that represents the following code:
+ * ```js
+ * (function (name, props) {
+ *  console.log(name + ' is ' + props.age);
+ * })("Bob",{"age":42})
+ * ```
+ * @note Be careful when passing objects as arguments, as unquoted property
+ * names may be changed during compilation.
+ * @param templateObj This contains the literal part of the template literal.
+ * @param emptyArgs Expressions that evaluate to the empty string to enable
+ *     inline comments.
+ */
+export function scriptWithArgs(
+    templateObj: TemplateStringsArray, ...emptyArgs: ReadonlyArray<''>):
+    (...argValues: Serializable[]) => TrustedScript {
+  if (emptyArgs.some(a => a !== '')) {
+    throw new Error(
+        'scriptWithArgs only allows empty string expressions ' +
+        'to enable inline comments.');
+  }
+  assertIsTemplateObject(
+      templateObj, true,
+      'scriptWithArgs is a template literal tag function ' +
+          'that only accepts template literals. ' +
+          'For example, scriptWithArgs`foo`;');
+  return (...argValues: Serializable[]) => {
+    const values = argValues.map(serializeAsScriptValue);
+    return createScript(`(${templateObj.join('')})(${values.join(',')})`);
+  };
 }
--- test/builders/script_builders_test.ts
+++ test/builders/script_builders_test.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import {concatScripts, script} from '../../src/builders/script_builders';
+import {concatScripts, script, scriptWithArgs} from '../../src/builders/script_builders';
 
 describe('script_builders', () => {
   describe('script', () => {
@@ -35,6 +35,41 @@ describe('script_builders', () => {
       const script1 = script`1;`;
       const script2 = script`2;`;
       expect(concatScripts(script1, script2).toString()).toEqual('1;2;');
+    });
+  });
+
+  describe('scriptWithArgs', () => {
+    it('can build a simple script with arguments',
+       () => {
+         expect(scriptWithArgs`function (a, b, c) {
+  console.log(a, b, c);
+}`(['hello', 123, null], 'test',
+   {'key': 'value'}).toString())
+             .toEqual(`(function (a, b, c) {
+  console.log(a, b, c);
+})(["hello",123,null],"test",{"key":"value"})`);
+       });
+
+    it('escapes < signs', () => {
+      expect(scriptWithArgs`alert`('</script</script').toString())
+          .toEqual(`(alert)("\\x3c/script\\x3c/script")`);
+    });
+
+    it('rejects any interpolation', () => {
+      const castScriptWithArgs =
+          scriptWithArgs as (arr: TemplateStringsArray, str: string) =>
+              (arg: string) => TrustedScript;
+      expect(() => castScriptWithArgs`${'console.log'}`('test'))
+          .toThrowError();
+    });
+
+    it('allows inline comments', () => {
+      expect(scriptWithArgs`function (a) {${/* Just a simple comment */ ''}
+  console.log(a);
+}`('arg').toString())
+          .toEqual(`(function (a) {
+  console.log(a);
+})("arg")`);
     });
   });
 });
