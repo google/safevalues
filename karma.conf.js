6ee20d499ab4af8d0b2a1bf28c88f4039c19602d
Damien Engels <engels@google.com>
2021-09-30 01:19:08 -0700

    No change
    PiperOrigin-RevId: 399873431

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,3 @@
-/**
- * @license
- * Copyright 2021 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
 module.exports = (config) => {
   config.set({
     frameworks: ['jasmine', 'karma-typescript'],
