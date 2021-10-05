729b88205c28eadf4680810ed1b13a93062cab5e
Damien Engels <engels@google.com>
2021-10-05 04:25:29 -0700

    Updates index.ts
    PiperOrigin-RevId: 400948915

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/**
- * @fileoverview Main safevalues module
- *
- * Exports types, constants and safe builders.
- */
-
-export {concatHtmls, createScriptSrc, htmlEscape} from './builders/html_builders';
+/** Safe builders */
+export {concatHtmls, createScriptSrc, htmlEscape, sanitizeHtml, sanitizeHtmlAssertUnchanged} from './builders/html_builders';
 export {concatScripts, script, scriptFromJson, scriptWithArgs} from './builders/script_builders';
 export {appendParams, blobUrlFromScript, replaceFragment, scriptUrl} from './builders/script_url_builders';
-/** Reexport the public type (but not the Impl). */
-export {EMPTY_HTML, unwrapHtmlForSink} from './implementation/html_impl';
-export {EMPTY_SCRIPT, unwrapScriptForSink} from './implementation/script_impl';
-export {unwrapScriptUrlForSink} from './implementation/script_url_impl';
+/** Types, constants and unwrappers */
+export {EMPTY_HTML, unwrapHtmlAsString, unwrapHtmlForSink} from './implementation/html_impl';
+export {EMPTY_SCRIPT, unwrapScriptAsString, unwrapScriptForSink} from './implementation/script_impl';
+export {unwrapScriptUrlAsString, unwrapScriptUrlForSink} from './implementation/script_url_impl';
