4391c8100b1afc7fa5548479594fdd88fd6e2f2d
ISE Hardening <noreply@google.com>
2021-12-07 16:00:30 -0800

    Safevalues: mark pure functions as pure in an outside-of-Google compatible way.
    PiperOrigin-RevId: 414849092

--- src/internals/pure.ts
+++ src/internals/pure.ts
@@ -3,13 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/**
- * Mark an expression as being free of side effects.
- *
- * This function exists to support Google compiler functionality. Most
- * compilers / bundlers will optimize it away.
- */
+/** Mark an expression as being free of side effects. */
 export function pure<T>(valueOf: () => T): T {
   // This odd looking expression is a workaround specific to the Closure
   // compiler to mark an expression as pure.
+  return ({valueOf}).valueOf();
 }
