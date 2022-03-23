c1baf1a36eed2c019b831d541835ca99b7c13b7c
Guillaume Weghsteen <gweg@google.com>
2022-03-23 06:36:42 -0700

    Internal change
    PiperOrigin-RevId: 436723208

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@
 
 /** Safe builders */
 export {concatHtmls, createScript, createScriptSrc, htmlEscape} from './builders/html_builders';
-export {appendParams, blobUrlFromScript, replaceFragment, scriptUrl} from './builders/resource_url_builders';
-export {concatScripts, script, scriptFromJson, scriptWithArgs} from './builders/script_builders';
+export {appendParams, blobUrlFromScript, replaceFragment, trustedResourceUrl} from './builders/resource_url_builders';
+export {concatScripts, safeScript, safeScriptWithArgs, scriptFromJson} from './builders/script_builders';
 /** Types, constants and unwrappers */
-export {EMPTY_HTML, isHtml, unwrapHtml, unwrapHtmlAsString} from './internals/html_impl';
-export {isResourceUrl, unwrapResourceUrl, unwrapResourceUrlAsString} from './internals/resource_url_impl';
-export {EMPTY_SCRIPT, isScript, unwrapScript, unwrapScriptAsString} from './internals/script_impl';
+export {EMPTY_HTML, isHtml, SafeHtml, unwrapHtml, unwrapHtmlAsString} from './internals/html_impl';
+export {isResourceUrl, TrustedResourceUrl, unwrapResourceUrl, unwrapResourceUrlAsString} from './internals/resource_url_impl';
+export {EMPTY_SCRIPT, isScript, SafeScript, unwrapScript, unwrapScriptAsString} from './internals/script_impl';
