241bb75c6c3618aef343fbcdf2ad77ed52212d85
Jakub Vrana <jakubvrana@google.com>
2022-09-20 03:43:35 -0700

    Allow setting href of SVGAElement.
    PiperOrigin-RevId: 475513960

--- src/dom/elements/anchor.ts
+++ src/dom/elements/anchor.ts
@@ -8,7 +8,7 @@ import {unwrapUrlOrSanitize, Url} from '../../builders/url_sanitizer';
 /**
  * Sets the Href attribute from the given Url.
  */
-export function setHref(anchor: HTMLAnchorElement, url: Url) {
+export function setHref(anchor: HTMLAnchorElement|SVGAElement, url: Url) {
   const sanitizedUrl = unwrapUrlOrSanitize(url);
   if (sanitizedUrl !== undefined) {
     anchor.href = sanitizedUrl;
