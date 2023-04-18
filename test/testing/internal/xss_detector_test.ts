cf8d528927475c0fe6ae469e667f1731fac65d55
Damien Engels <engels@google.com>
2023-04-18 05:20:59 -0700

    # Creates a generic test utility to detect if an XSS can be triggered
    This is very useful when writing tests that would like to assure no XSS gets triggered when processing user input.
    
    PiperOrigin-RevId: 525116878

---
+++ test/testing/internal/xss_detector_test.ts
@@ -0,0 +1,117 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {safeGlobal, safeRange, safeScriptEl} from 'safevalues/dom';
+
+import {testonlyHtml, testonlyResourceUrl, testonlyScript} from '../conversions';
+
+import {XSSDetector} from './xss_detector';
+
+describe('XSSDetector', () => {
+  it('triggers synchronously for eval ', async () => {
+    const detector = new XSSDetector();
+    safeGlobal.globalEval(window, testonlyScript(detector.payload));
+    expect(detector.wasTriggered()).toBe(true);
+  });
+
+  it('times out when not triggered', async () => {
+    const detector = new XSSDetector();
+
+    // Access payload to make sure no error is thrown.
+    expect(detector.payload).toBeTruthy();
+
+    expect(await detector.waitForTrigger()).toBe(false);
+  });
+
+  it('triggers synchronously for inline script', async () => {
+    const detector = new XSSDetector();
+
+    const script = document.createElement('script');
+    safeScriptEl.setTextContent(script, testonlyScript(detector.payload));
+    document.body.appendChild(script);
+    document.body.removeChild(script);
+
+    expect(detector.wasTriggered()).toBe(true);
+  });
+
+  it('triggers asynchronously for script with src', async () => {
+    const detector = new XSSDetector();
+
+    const script = document.createElement('script');
+    const url = testonlyResourceUrl(`data:text/javascript,${detector.payload}`);
+    safeScriptEl.setSrc(script, url);
+    document.body.appendChild(script);
+    document.body.removeChild(script);
+
+    expect(detector.wasTriggered()).toBe(false);
+
+    expect(await detector.waitForTrigger()).toBe(true);
+  });
+
+  it('triggers synchronously for inline script parsed with Range', () => {
+    const detector = new XSSDetector();
+
+    const html = testonlyHtml(`<script>${detector.payload}<${'/'}script>`);
+    const range = document.createRange();
+    const script = safeRange.createContextualFragment(range, html).firstChild!;
+    document.body.appendChild(script);
+    document.body.removeChild(script);
+
+    expect(detector.wasTriggered()).toBe(true);
+  });
+
+  it('triggers asynchronously when parsing img with onerror handler',
+     async () => {
+       const detector = new XSSDetector();
+
+       // The bad-scheme is important to make sure the load fails without having
+       // to hit the newtork stack, which would make the test flaky due to the
+       // variable time it takes to trigger the onerror handler.
+       const html =
+           testonlyHtml(`<img src=bad-scheme:_ onerror="${detector.payload}">`);
+       const range = document.createRange();
+       safeRange.createContextualFragment(range, html);
+
+       expect(detector.wasTriggered()).toBe(false);
+
+       expect(await detector.waitForTrigger()).toBe(true);
+     });
+
+  it('can be instantiated multiple times in parallel', async () => {
+    const detector1 = new XSSDetector();
+    const detector2 = new XSSDetector();
+    const detector3 = new XSSDetector();
+
+    // Access payloads to avoid having the detector complaining
+    expect(detector1.payload).toBeTruthy();
+    expect(detector2.payload).toBeTruthy();
+    expect(detector3.payload).toBeTruthy();
+
+    expect(detector1.wasTriggered()).toBe(false);
+    expect(detector2.wasTriggered()).toBe(false);
+    expect(detector3.wasTriggered()).toBe(false);
+
+    safeGlobal.globalEval(window, testonlyScript(detector2.payload));
+    setTimeout(() => {
+      safeGlobal.globalEval(window, testonlyScript(detector3.payload));
+    }, 0);
+
+    expect(detector1.wasTriggered()).toBe(false);
+    expect(detector2.wasTriggered()).toBe(true);
+    expect(detector3.wasTriggered()).toBe(false);
+
+    expect(await detector1.waitForTrigger()).toBe(false);
+    expect(await detector2.waitForTrigger()).toBe(true);
+    expect(await detector3.waitForTrigger()).toBe(true);
+  });
+
+  it('throws an error if the detector is checked whith an unused payload',
+     async () => {
+       const detector = new XSSDetector();
+
+       expect(() => detector.wasTriggered()).toThrow();
+       await expectAsync(detector.waitForTrigger()).toBeRejected();
+     });
+});
---
+++ testing/internal/xss_detector.ts
@@ -0,0 +1,75 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Helper to detect if something should trigger an XSS or not,
+ * this allows us to test that our code is not triggering XSS.
+ */
+export class XSSDetector {
+  // Random id to make sure this detector is unique.
+  private readonly RANDOM_ID = Math.random().toString(16).slice(2);
+
+  // Conveniently casted version of window.
+  private readonly global =
+      window as unknown as {[x: string]: Function | undefined};
+
+  // A promise to wait for things to load.
+  private readonly promise: Promise<boolean>;
+
+  // Whether this detector was triggered.
+  private triggered = false;
+
+  // Whether the payload was accessed, as a fail safe to make sure the test
+  // doesn't accidentally forget to inject the payload.
+  private payloadAccessed = false;
+
+  constructor() {
+    this.promise = new Promise<boolean>((resolve) => {
+      // Wait some time, and assume the XSS will not trigger if it timesout.
+      const timeoutId = setTimeout(() => {
+        this.cleanup();
+        resolve(false);
+      }, 200);
+
+      // NOTE: The 200ms wait time is a bit arbitrary, but seems to be large
+      // enough for all the XSS in the tests to trigger. Increase it if you're
+      // adding new vectors that don't reliably trigger.
+
+      // If it does trigger, clear the timeout
+      this.global[`XSS_${this.RANDOM_ID}`] = () => {
+        this.cleanup();
+        clearTimeout(timeoutId);
+        this.triggered = true;
+        resolve(true);
+      };
+    });
+  }
+
+  private cleanup() {
+    delete this.global[`XSS_${this.RANDOM_ID}`];
+  }
+
+  /** The payload to use in the XSS vector. */
+  get payload(): string {
+    this.payloadAccessed = true;
+    return `XSS_${this.RANDOM_ID}()`;
+  }
+
+  private ensurePayloadAccessed() {
+    if (!this.payloadAccessed) {
+      throw new Error('Payload was never accessed');
+    }
+  }
+
+  wasTriggered(): boolean {
+    this.ensurePayloadAccessed();
+    return this.triggered;
+  }
+
+  async waitForTrigger(): Promise<boolean> {
+    this.ensurePayloadAccessed();
+    return this.promise;
+  }
+}
