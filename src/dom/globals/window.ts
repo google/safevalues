56278f779e155f9200052a2107870f7cfbd5100a
Guillaume Weghsteen <gweg@google.com>
2023-11-28 04:50:12 -0800

    Exports `getStyleNonce` on `safeWindow`.
    PiperOrigin-RevId: 585932310

--- src/dom/globals/window.ts
+++ src/dom/globals/window.ts
@@ -24,15 +24,26 @@ export function open(
 
 /** Returns CSP nonce, if set for any script tag. */
 export function getScriptNonce(win: Window): string {
+  return getNonceFor('script', win);
+}
+
+/** Returns CSP nonce, if set for any style tag. */
+export function getStyleNonce(win: Window): string {
+  return getNonceFor('style', win);
+}
+
+function getNonceFor(elementName: 'script' | 'style', win: Window): string {
   const doc = win.document;
   // document.querySelector can be undefined in non-browser environments.
-  const script = doc.querySelector?.<HTMLScriptElement>('script[nonce]');
-  if (script) {
+  const el = doc.querySelector?.<HTMLScriptElement | HTMLStyleElement>(
+    `${elementName}[nonce]`,
+  );
+  if (el) {
     // Try to get the nonce from the IDL property first, because browsers that
     // implement additional nonce protection features (currently only Chrome) to
     // prevent nonce stealing via CSS do not expose the nonce via attributes.
     // See https://github.com/whatwg/html/issues/2369
-    return script['nonce'] || script.getAttribute('nonce') || '';
+    return el['nonce'] || el.getAttribute('nonce') || '';
   }
   return '';
 }
