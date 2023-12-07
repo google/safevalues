b07409bc3edcf2cae0da19670b1859be8888a02b
Guillaume Weghsteen <gweg@google.com>
2023-12-07 04:20:10 -0800

    Internal Code Change
    PiperOrigin-RevId: 588743063

--- test/restricted/reviewed_test.ts
+++ test/restricted/reviewed_test.ts
@@ -8,15 +8,17 @@ import {htmlSafeByReview} from '../../src/restricted/reviewed';
 describe('reviewed conversions', () => {
   it('require a justification', () => {
     expect(() => {
-      htmlSafeByReview('aaa', null as unknown as string);
+      htmlSafeByReview('aaa', {justification: null as unknown as string});
     }).toThrowError(/A justification must be provided/);
 
     expect(() => {
-      htmlSafeByReview('aaa', '   ');
+      htmlSafeByReview('aaa', {justification: '   '});
     }).toThrowError(/A justification must be provided/);
 
-    expect(htmlSafeByReview('aaa', 'This is just a test').toString()).toEqual(
-      'aaa',
-    );
+    expect(
+      htmlSafeByReview('aaa', {
+        justification: 'This is just a test',
+      }).toString(),
+    ).toEqual('aaa');
   });
 });
