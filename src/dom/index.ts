20f000f1bf021ef88ddb8823456c24ae05b7daa2
Guillaume Weghsteen <gweg@google.com>
2024-11-15 09:17:58 -0800

    Delete legacy safevalues/dom exports safeAreaEl, safeBaseEl, safeEmbedEl, safeSvgEl, safeSvgUseEl, safeDocument, safeRange.
    PiperOrigin-RevId: 696909422

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -11,11 +11,8 @@
 
 // Old names. These are deprecated. Use the function directly defined on safevalues.dom instead.
 export * as safeAnchorEl from './elements/anchor.js';
-export * as safeAreaEl from './elements/area.js';
-export * as safeBaseEl from './elements/base.js';
 export * as safeButtonEl from './elements/button.js';
 export * as safeElement from './elements/element.js';
-export * as safeEmbedEl from './elements/embed.js';
 export * as safeFormEl from './elements/form.js';
 export * as safeIframeEl from './elements/iframe.js';
 export * as safeInputEl from './elements/input.js';
@@ -23,13 +20,8 @@ export * as safeLinkEl from './elements/link.js';
 export * as safeObjectEl from './elements/object.js';
 export * as safeScriptEl from './elements/script.js';
 export * as safeStyleEl from './elements/style.js';
-export * as safeSvgEl from './elements/svg.js';
-export * as safeSvgUseEl from './elements/svg_use.js';
-export * as safeDocument from './globals/document.js';
 export * as safeDomParser from './globals/dom_parser.js';
-export * as safeGlobal from './globals/global.js';
 export * as safeLocation from './globals/location.js';
-export * as safeRange from './globals/range.js';
 export * as safeServiceWorkerContainer from './globals/service_worker_container.js';
 export * as safeUrl from './globals/url.js';
 export * as safeWindow from './globals/window.js';
