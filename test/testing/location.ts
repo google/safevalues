2c281ff0083c3bc95cd1ce469915944669aec008
Guillaume Weghsteen <gweg@google.com>
2024-12-19 01:21:09 -0800

    No public description
    PiperOrigin-RevId: 707824634

---
+++ test/testing/location.ts
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Re-export the location wrapper for tests to observe and mock
+ * it.
+ */
+
+export {mockableLocation} from '../dom/globals/location';
